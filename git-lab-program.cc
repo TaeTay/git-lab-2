d8f29cd442d0f7e728a97987a521603e8ad221bd
Taylor Thomas <tt680321@ohio.edu>
2023-09-06 17:20:28 -0400

    Finished program

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+* File: git-lab-program.cc
+*Author: Taylor Thomas
+*Date: 9/6/23
+*Description: Following the steps on the git hub assignment
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}//main
